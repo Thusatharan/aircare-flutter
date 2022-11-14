03505b0094bbda581dd30c426f2c9351dd0e6562
Thusatharan <thusatharan27@gmail.com>
2022-11-14 08:39:44 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:aircare/components/bottom_navigation.dart';
 import 'package:aircare/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -27,7 +30,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         textTheme: TextTheme(subtitle2: TextStyle(color: Colors.white)),
       ),
-      home: const BottomNavigation(),
+      home: const HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,77 +5,119 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:lottie/lottie.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Container(
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Image(image: AssetImage('aseets/images/aircare_logo.jpeg')),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 200,
-                        padding: EdgeInsets.all(15),
-                        width: MediaQuery.of(context).size.width / 2,
-                        child: InkWell(
-                          onTap: () => Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => VideoUpload())),
-                          child: Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10),
-                                boxShadow: [
-                                  BoxShadow(
-                                    color: Color.fromARGB(255, 3, 108, 168)
-                                        .withOpacity(0.5),
-                                    spreadRadius: 3,
-                                    blurRadius: 7,
-                                    offset: Offset(
-                                        0, 3), // changes position of shadow
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: [
+            Color.fromARGB(255, 165, 155, 219),
+            Color.fromARGB(255, 181, 228, 196),
+          ],
+        ),
+      ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          body: SafeArea(
+            child: Container(
+                width: double.infinity,
+                child: Stack(
+                  children: [
+                    Lottie.asset('aseets/images/cctv.json'),
+                    Column(
+                      children: [
+                        Expanded(child: Container()),
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 100,
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(40)),
+                                width: MediaQuery.of(context).size.width / 2,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                EldersCare()));
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color.fromARGB(255, 0, 71, 133)),
                                   ),
-                                ],
-                                color: Color.fromARGB(255, 4, 132, 206),
+                                  child: Image.asset(
+                                    ("aseets/images/elder.png"),
+                                    // AssetImage),
+                                  ),
+                                ),
                               ),
-                              child: Column(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: Icon(
-                                      Icons.accessibility,
-                                      size: 70,
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                    ),
+                              Container(
+                                height: 100,
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(40)),
+                                width: MediaQuery.of(context).size.width / 2,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                EldersCare()));
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color.fromARGB(255, 0, 71, 133)),
                                   ),
-                                  Text(
-                                    'Elders Care',
-                                    style:
-                                        Theme.of(context).textTheme.subtitle2,
+                                  child: Image.asset(
+                                    ("aseets/images/sleep.png"),
+                                    // AssetImage),
                                   ),
-                                ],
-                              )),
+                                ),
+                              ),
+                              Container(
+                                // height: 100,
+                                padding: EdgeInsets.all(15),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(40)),
+                                width: MediaQuery.of(context).size.width / 2,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) =>
+                                                EldersCare()));
+                                  },
+                                  style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Color.fromARGB(255, 0, 71, 133)),
+                                  ),
+                                  child: Image.asset(
+                                    ("aseets/images/care.png"),
+                                    // AssetImage),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-            ],
+                      ],
+                    ),
+                  ],
+                )),
           )),
-    ));
+    );
   }
 }
